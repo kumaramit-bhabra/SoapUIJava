cb579de62a41fca0601c63ea4a57cf5102b6ef5d
kumaramit-bhabra <Loadrunner_1>
2020-01-12 18:24:25 +0000

    First commit for SoapUI integration with Java

---
+++ src/test/java/SoapUI/SoapIntegration.java
@@ -0,0 +1,37 @@
+package SoapUI;
+
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.impl.wsdl.WsdlTestSuite;
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+import com.eviware.soapui.model.support.PropertiesMap;
+import com.eviware.soapui.model.testsuite.TestRunner;
+import com.eviware.soapui.model.testsuite.TestRunner.Status;
+import com.eviware.soapui.support.SoapUIException;
+import org.apache.xmlbeans.XmlException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class SoapIntegration
+{
+    @Test
+    public void soapTest() throws IOException, SoapUIException, XmlException {
+        //EmployeePortal-soapui-project.xml - SOAP Request, EmployeeTestSuite
+        //Library-Books-soapui-project.xml - REST Request, E2E - Add Find Delete Book
+
+        WsdlProject project = new WsdlProject("C:\\Users\\Admin\\Documents\\SoapUI\\Library-Books-soapui-project.xml");
+        WsdlTestSuite testSuite = project.getTestSuiteByName("E2E - Add Find Delete Book");
+        System.out.println("Hello Count is "+ testSuite.getTestCaseCount());
+        for(int i = 0 ; i < testSuite.getTestCaseCount(); i++)
+        {
+            WsdlTestCase testCase = testSuite.getTestCaseAt(i);
+            TestRunner runner = testCase.run(new PropertiesMap(),false);
+            System.out.println("status is " +runner.getStatus());
+            Assert.assertEquals(Status.FINISHED,runner.getStatus());
+        }
+
+        System.out.println("Hello World");
+    }
+
+}
