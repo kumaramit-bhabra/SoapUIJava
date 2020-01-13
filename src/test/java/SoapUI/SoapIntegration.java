7625e85fa7ee40131790b5c0c1018cb857576428
kumaramit-bhabra <55563230+kumaramit-bhabra@users.noreply.github.com>
2020-01-13 15:05:38 +0000

    Update SoapIntegration.java

--- src/test/java/SoapUI/SoapIntegration.java
+++ src/test/java/SoapUI/SoapIntegration.java
@@ -24,8 +24,8 @@ public class SoapIntegration
         // WsdlProject project = new WsdlProject("C:\\Users\\Admin\\Documents\\SoapUI\\Library-Books-soapui-project.xml");
 
         //This is for Jenkins
-        //WsdlProject project = new WsdlProject("Library-Books-soapui-project.xml");
-        WsdlProject project = new WsdlProject(".//..//..//SoapUI/Library-Books-soapui-project.xml");
+        WsdlProject project = new WsdlProject("Library-Books-soapui-project.xml");
+       
         
         WsdlTestSuite testSuite = project.getTestSuiteByName("E2E - Add Find Delete Book");
         System.out.println("Hello Count is "+ testSuite.getTestCaseCount());
